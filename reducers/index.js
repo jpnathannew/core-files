e7a1a53a6f628108c755732cc71ba42071b50f87
m_tap <tapeshmittal29@gmail.com>
2021-03-09 16:07:49 +0530

    removed persist from core files

--- reducers/index.js
+++ reducers/index.js
@@ -1,16 +1,9 @@
 import { combineReducers } from "redux";
-import { persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+
 import Test from "./Test";
 
-const persistConfig = {
-    key: "rootState",
-    storage,
-    whitelist: [],
-};
-
 const RootReducer = combineReducers({
-    Test,
+  Test,
 });
 
-export default persistReducer(persistConfig, RootReducer);
+export default RootReducer;
